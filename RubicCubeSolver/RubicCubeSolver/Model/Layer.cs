99baaf8a1d83285e334912f5d3ff7e484230872d
tnizicki <tomek.nizicki@gmail.com>
2015-12-12 19:22:35 +0100

    Binding change

--- RubicCubeSolver/RubicCubeSolver/Model/Layer.cs
+++ RubicCubeSolver/RubicCubeSolver/Model/Layer.cs
@@ -4,42 +4,64 @@ using System.Collections.Generic;
 using System.Windows.Input;
 using System.Windows.Media;
 using System;
+using Utils;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace RubicCubeSolver.Model
 {
-    public class Layer
+    public class Layer : BindableBase
     {
+        private Color _topLeft;
+        private Color _topMid;
+        private Color _topRight;
+        private Color _midLeft;
+        private Color _mid;
+        private Color _midRight;
+        private Color _bottomLeft;
+        private Color _bottomMid;
+        private Color _bottomRight;
+
+        private LinkedList<Color> _colorsOrder = new LinkedList<Color>(); //{ Colors.White, Colors.Yellow, Colors.Orange, Colors.Red, Colors.Green, Colors.Blue };
+
         public ICommand ChangeColorCommand { get; set; }
 
         public Layer()
         {
-            _content = new Dictionary<LayerPosition, Color>()
-            {
-                {LayerPosition.BottomLeft, Colors.Orange },
-                {LayerPosition.BottomMid,  Colors.Orange },
-                {LayerPosition.BottomRight,  Colors.Orange },
-                {LayerPosition.MidLeft,  Colors.Orange },
-                {LayerPosition.Mid,  Colors.Orange },
-                {LayerPosition.MidRight,  Colors.Orange },
-                {LayerPosition.TopLeft,  Colors.Orange },
-                {LayerPosition.TopMid,  Colors.Orange },
-                {LayerPosition.TopRight,  Colors.Orange }
-            };
+            TopLeft = Colors.Orange;
+            TopMid = Colors.Orange;
+            TopRight = Colors.Orange;
+            MidLeft = Colors.Orange;
+            Mid = Colors.Orange;
+            MidRight = Colors.Orange;
+            BottomLeft = Colors.Orange;
+            BottomMid = Colors.Orange;
+            BottomRight = Colors.Orange;
 
             ChangeColorCommand = new DelegateCommand<string>(ChangeColor);
+
+            _colorsOrder.AddLast(Colors.White);
+            _colorsOrder.AddLast(Colors.Yellow);
+            _colorsOrder.AddLast(Colors.Orange);
+            _colorsOrder.AddLast(Colors.Red);
+            _colorsOrder.AddLast(Colors.Green);
+            _colorsOrder.AddLast(Colors.Blue);
         }
 
-        private void ChangeColor(object obj)
+        private void ChangeColor(string parameter)
         {
-            throw new NotImplementedException();
+            var property = GetType().GetProperty(parameter);
+            var nextNode = _colorsOrder.Find((Color)property.GetValue(this)).Next;
+            var nextVal = nextNode != null ? nextNode.Value : _colorsOrder.First.Value;
+            property.SetValue(this, nextVal);
         }
-
-        private readonly Dictionary<LayerPosition, Color> _content;
 
         public Layer(Dictionary<LayerPosition, Color> content)
             : this()
         {
-            _content = content;
+            foreach (var item in content)
+            {
+                GetType().GetProperty(item.Key.ToString()).SetValue(this, item.Value);
+            }
         }
 
         internal static Layer GetFullColorLayer(Color color)
@@ -60,55 +82,55 @@ namespace RubicCubeSolver.Model
 
         public Color TopLeft
         {
-            get { return _content[LayerPosition.TopLeft]; }
-            set { _content[LayerPosition.TopLeft] = value; }
+            get { return _topLeft; }
+            set { SetProperty(ref _topLeft, value); }
         }
 
         public Color TopMid
         {
-            get { return _content[LayerPosition.TopMid]; }
-            set { _content[LayerPosition.TopMid] = value; }
+            get { return _topMid; }
+            set { SetProperty(ref _topMid, value); }
         }
         public Color TopRight
         {
-            get { return _content[LayerPosition.TopRight]; }
-            set { _content[LayerPosition.TopRight] = value; }
+            get { return _topRight; }
+            set { SetProperty(ref _topRight, value); }
         }
 
         public Color MidLeft
         {
-            get { return _content[LayerPosition.MidLeft]; }
-            set { _content[LayerPosition.MidLeft] = value; }
+            get { return _midLeft; }
+            set { SetProperty(ref _midLeft, value); }
         }
 
         public Color Mid
         {
-            get { return _content[LayerPosition.Mid]; }
-            set { _content[LayerPosition.Mid] = value; }
+            get { return _mid; }
+            set { SetProperty(ref _mid, value); }
         }
 
         public Color MidRight
         {
-            get { return _content[LayerPosition.MidRight]; }
-            set { _content[LayerPosition.MidRight] = value; }
+            get { return _midRight; }
+            set { SetProperty(ref _midRight, value); }
         }
 
         public Color BottomLeft
         {
-            get { return _content[LayerPosition.BottomLeft]; }
-            set { _content[LayerPosition.BottomLeft] = value; }
+            get { return _bottomLeft; }
+            set { SetProperty(ref _bottomLeft, value); }
         }
 
         public Color BottomMid
         {
-            get { return _content[LayerPosition.BottomMid]; }
-            set { _content[LayerPosition.BottomMid] = value; }
+            get { return _bottomMid; }
+            set { SetProperty(ref _bottomMid, value); }
         }
 
         public Color BottomRight
         {
-            get { return _content[LayerPosition.BottomRight]; }
-            set { _content[LayerPosition.BottomRight] = value; }
+            get { return _bottomRight; }
+            set { SetProperty(ref _bottomRight, value); }
         }
     }
 }
---
+++ RubicCubeSolver/Utils/EnumUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class EnumUtils
+    {
+        public static T ParseEnum<T>(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
